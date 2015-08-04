3832585ea1403cfa21aabfa1751fd5a547685b9d
Lisyk <sergey.liskovoy@gmail.com>
2015-08-04 15:38:50 -0400

    add method for quit and tweeting

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -5,5 +5,35 @@ class MicroBlogger
 
   def initialize
     puts "Initializing MicroBlogger"
+    @client = JumpstartAuth.twitter
+  end
+
+  def tweet(message)
+    if message.length > 140
+      puts "Your tweet is too long"
+    else
+      @client.update(message)
+    end
+  end
+
+  def run
+    puts "Welcome to the JSL Twitter Client!"
+    command=""
+    while command != 'q'
+      print 'enter command: '
+      input = gets.chomp
+      parts = input.split(" ")
+      command = parts[0]
+      case command
+      when 'q' then puts 'Goodbye!'
+      when 't' then tweet(parts[1..-1].join(" "))
+      else
+        puts "Sorry, I don't know how to #{command}"
+      end
+    end
   end
 end
+
+blogger = MicroBlogger.new
+blogger.tweet("MicroBlogger Initialized third tweet")
+blogger.run
